4eaca05592bb84b7365662115134d344d7bef3cc
Vladimir Petukhov <VladimirPetukhov@students.softuni.bg>
2021-05-30 18:17:40 +0300

    ADD: c# algs

---
+++ C#/01.LinearSearch/LinearSearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.LinearSearch
+{
+    public class LinearSearch
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 4, 5, 8, 9, 44, 56, 32, 11, 2, 3, 0, 88, 44, 33, 1, 6, 56, 13, 23, 55 };
+
+            Console.WriteLine(Linear_Search(array,44));
+        }
+
+        static public int? Linear_Search(int[] array, int item)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                count++;
+                if(array[i] == item){
+                    return i;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ C#/02.BubbleSearch/BinarySearch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02.BubbleSearch
+{
+    public class BinarySearch
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27.28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43 };
+            Console.WriteLine(BinarySearch(array, 20));
+        }
+
+        static int Binary_Search(int[] array, int item)
+        {
+            var start = 0;
+            var end = array.Length;
+            var middle;
+            var found = false;
+            var positon = -1;
+            while (found == false && start <= end)
+            {
+                count++;
+                middle = Math.Floor((start + end) / 2);
+                if (array[middle] == item)
+                {
+                    found = true;
+                    positon = middle;
+                    return positon;
+                }
+                if (item < array[middle])
+                {
+                    end = middle - 1;
+                }
+                else
+                {
+                    start = middle + 1;
+                }
+            }
+            return positon;
+        }
+    }
+}
