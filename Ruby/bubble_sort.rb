c7541083eb1c0a60db1442fbce1c3133ab9a8cfd
vpetukhov <vladimir.petukhov@elando.com>
2021-11-11 15:28:38 +0200

    ADD: merge sort

--- Ruby/bubble_sort.rb
+++ Ruby/bubble_sort.rb
@@ -1,19 +1,19 @@
 def bubble_sort(array)
-  n=array.length
+  n = array.length
 
   loop do
-    swapped=false
+    swapped = false
 
-    (n-1).times do |i|
-      if array[i]>array[i+1]
-        array[i],array[i+1]=array[i+1],array[i]
-        swapped=true
+    (n - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
       end
     end
-    break  if not swapped
+    break if not swapped
   end
   array
 end
 
-a=[3,2,5,1,7,5,9,0,2,11,3,1]
+a = [3, 2, 5, 1, 7, 5, 9, 0, 2, 11, 3, 1]
 p bubble_sort(a)
--- Ruby/quick_sort.rb
+++ Ruby/quick_sort.rb
@@ -2,12 +2,12 @@ class Array
   def quicksort
     return [] if empty?
 
-    pivot=delete_at(size/2)
-    left,right=partition(&pivot.method(:>))
+    pivot = delete_at(size / 2).floor
+    left, right = partition(&pivot.method(:>))
 
-    return *left.quicksort,pivot,*right.quicksort
+    return *left.quicksort, pivot, *right.quicksort
   end
 end
 
-arr=[3,2,5,1,7,5,9,0,2,11,3,1]
+arr = [3, 2, 5, 1, 7, 5, 9, 0, 2, 11, 3, 1]
 p arr.quicksort
